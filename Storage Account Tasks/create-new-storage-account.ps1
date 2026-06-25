6ef6703849cfd2e76164dbd7040d4ea51491492f
stevenshade90 <54482297+stevenshade90@users.noreply.github.com>
2026-06-25 15:24:20 -0400

    Add script to create a new Azure storage account

---
+++ Storage Account Tasks/create-new-storage-account.ps1
@@ -0,0 +1,7 @@
+$ResourceGroup = "<RG where the storage account will be stored>"
+$Location = "<EastUs, WestUs..."
+$StorageAccountName = "<unique name for storage account"
+$SkuName = "Standard_LRS <or other storage tier>"
+
+New-AzStorageAccount -Name $StorageAccountName -ResourceGroupName $ResourceGroup -Location $Location -SkuName $SkuName -AllowBlobPublicAccess $False
+
