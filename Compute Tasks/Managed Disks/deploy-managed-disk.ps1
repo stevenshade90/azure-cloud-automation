f6b922dbf970d2ee878bdf6c187d452bc068d3f4
stevenshade90 <54482297+stevenshade90@users.noreply.github.com>
2026-09-17 16:42:33 -0400

    Delete Compute Tasks/Managed Disks directory

--- Compute Tasks/Managed Disks/deploy-managed-disk.ps1
+++
@@ -1,7 +0,0 @@
-Connect-AzAccount
-
-New-AzResourceGroup -Name 'rg-sandbox-disks' -Location 'eastus'
-
-New-AzResourceGroupDeployment `
-  -ResourceGroupName 'rg-sandbox-disks' `
-  -TemplateFile '.\create-managed-disk.bicep'
